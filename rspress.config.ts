1fe1df6a4ed5ebed67b41b6a532e5213a36f8aaf
xitiantian <tiantian.xi@aminer.cn>
2024-08-22 16:40:00 +0800

    init

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,19 +1,17 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'self',
-  base:'/self/',
-  description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
+  root: path.join(__dirname, "docs"),
+  title: "self",
+  base: "/self/",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
   // logo: {
   //   light: '/rspress-light-logo.png',
   //   dark: '/rspress-dark-logo.png',
   // },
   themeConfig: {
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
-    ],
+    socialLinks: [{ icon: "github", mode: "link", content: "https://github.com/web-infra-dev/rspress" }],
   },
 });
